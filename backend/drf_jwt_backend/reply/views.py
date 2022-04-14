280a458d2e0474fcb66efabe7c0f915801dbd0c7
rexsheikh <56654312+rexsheikh@users.noreply.github.com>
2022-04-14 15:34:20 -0400

    Finished the post reply functionality that takes in a comment id and posts a text reply.

--- backend/drf_jwt_backend/reply/views.py
+++ backend/drf_jwt_backend/reply/views.py
@@ -1,3 +1,5 @@
+from xmlrpc.client import ResponseError
+from django.forms import modelformset_factory
 from django.shortcuts import render
 from logging import raiseExceptions
 from xml.etree.ElementTree import Comment
@@ -8,14 +10,35 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from .models import Reply
 from .serializers import ReplySerializer
+from comments.models import Comments
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET','POST'])
 @permission_classes([IsAuthenticated])
 def replies(request,pk):
     if request.method == 'GET':
         replies = Reply.objects.filter(comment = pk)
         serializer = ReplySerializer(replies, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        comment = get_object_or_404(Comments,pk=pk)
+        serializer = ReplySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(comment = comment,user_id = request.user.id)
+            return Response(serializer.data,status = status.HTTP_201_CREATED)
+        return ResponseError(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
 
-    # elif request.method = "POST"
+        
+
+
+
+        
+
+
+
+#     if request.method == 'POST':
+#     serializer = CommentsSerializer(data=request.data)
+# if serializer.is_valid():
+#     serializer.save(user=request.user)
+#     return Response(serializer.data, status=status.HTTP_201_CREATED)
+# return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
