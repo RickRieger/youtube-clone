2598d866b02e287bdb8b968c72440a37e3d4aa5e
Rick Rieger <rickrieger36251@gmail.com>
2022-04-15 18:07:11 -0400

    Modified nav bar to resmeble youtube navbar + added mui icons

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,25 +1,57 @@
-import React from "react";
-import { useContext } from "react";
-import { useNavigate, Link } from "react-router-dom";
-import AuthContext from "../../context/AuthContext";
-import "./NavBar.css";
+import React from 'react';
+import { useContext } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import AuthContext from '../../context/AuthContext';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import VideoCallOutlinedIcon from '@material-ui/icons/VideoCallOutlined';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import AppsIcon from '@material-ui/icons/Apps';
+import Avatar from '@material-ui/core/Avatar';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+
+import './NavBar.css';
 
 const Navbar = () => {
   const { logoutUser, user } = useContext(AuthContext);
   const navigate = useNavigate();
   return (
-    <div className="navBar">
+    <div className='navBar'>
       <ul>
-        <li className="brand">
-          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-            <b>React/Django JWT</b>
+        <li className='brand'>
+          <Link to='/' style={{ textDecoration: 'none', color: 'white' }}>
+            <MenuIcon />
           </Link>
+          <img
+            className='logo-img'
+            src='https://upload.wikimedia.org/wikipedia/commons/a/af/Youtube.png'
+            alt='youtube image'
+          />
         </li>
-        <li>
+
+        <li className="center-nav-cluster">
+          <input type='text' className='search-field'/>
+          <SearchIcon style = {{ color : "white"}} className='search-button'/>
+        </li>
+
+        <li></li>
+        <li className='right-nav-cluster'>
           {user ? (
-            <button onClick={logoutUser}>Logout</button>
+            <div>
+              <VideoCallOutlinedIcon /> <AppsIcon /> <NotificationsIcon />{' '}
+            </div>
           ) : (
-            <button onClick={() => navigate("/login")}>Login</button>
+            <div>
+              {' '}
+              <AppsIcon /> <MoreVertIcon />
+            </div>
+          )}
+
+          {user ? (
+            <Avatar onClick={logoutUser} />
+          ) : (
+            <button onClick={() => navigate('/login')}>Sign In</button>
           )}
         </li>
       </ul>
