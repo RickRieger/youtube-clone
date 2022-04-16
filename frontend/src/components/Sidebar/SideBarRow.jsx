b664b5df9b519d8ac95604f2eeb565fca2d7e3a7
Rick Rieger <rickrieger36251@gmail.com>
2022-04-16 13:54:43 -0400

    Added more css to side bar+ added more icons and text to side bar

--- frontend/src/components/Sidebar/SideBarRow.jsx
+++ frontend/src/components/Sidebar/SideBarRow.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './SideBarRow.css'
-function SideBarRow({ title, Icon }) {
+function SideBarRow({ selected, title, Icon }) {
   return (
-    <div className='sidebar__row'>
+    <div className={`sidebar__row ${selected && 'selected'}`}>
       <Icon className="sidebar__row__icon"/>
       <h2  className="sidebar__row__title">{title}</h2>
     </div>
--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import SideBarRow from './SideBarRow'
+import HomeIcon from '@material-ui/icons/Home'
 import WhatshotIcon from '@material-ui/icons/Whatshot';
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
-import HomeIcon from '@material-ui/icons/Home';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';import ThumbUpAltOutLinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import OndemandVideoIcon from '@material-ui/icons/OndemandVideo';import HistoryIcon from '@material-ui/icons/History';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
 
 import './Sidebar.css'
 function Sidebar() {
   return (
     <div className="sidebar">
-      <SideBarRow title="Home" Icon={HomeIcon}/>
+      <SideBarRow selected title="Home" Icon={HomeIcon}/>
       <SideBarRow title="Trending" Icon={WhatshotIcon}/>
       <SideBarRow title="Subscription" Icon={SubscriptionsIcon}/>
+      <hr />
+      <SideBarRow title="Library" Icon={VideoLibraryIcon}/>
+      <SideBarRow title="History" Icon={HistoryIcon}/>
+      <SideBarRow title="Your Videos" Icon={OndemandVideoIcon}/>
+      <SideBarRow title="Watch Later" Icon={WatchLaterIcon}/>
+      <SideBarRow title="Liked Videos" Icon={ThumbUpAltOutLinedIcon}/>
+      <SideBarRow title="Show more" Icon={ExpandMoreIcon}/>
+       <hr />
+
     </div>
   )
 }
