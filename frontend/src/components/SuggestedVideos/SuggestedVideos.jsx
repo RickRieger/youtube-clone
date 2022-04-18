6125f6b90e7433436ae3d0b8de7e55a0df778266
Rick Rieger <rickrieger36251@gmail.com>
2022-04-18 15:25:12 -0400

    Added axios call to youtube API + displaqyed data in the suggested videos component passing props down to video card component

--- frontend/src/components/SuggestedVideos/SuggestedVideos.jsx
+++ frontend/src/components/SuggestedVideos/SuggestedVideos.jsx
@@ -1,21 +1,54 @@
-import React from 'react'
-import './SuggestedVideos.css'
-import Video from '../Video/Video'
+import React, { useEffect, useState } from 'react';
+import './SuggestedVideos.css';
+import Video from '../Video/Video';
+import axios from 'axios';
 function SuggestedVideos() {
-  return (
-    <div className="suggested__videos">
-      <h2> Suggested Videos</h2>
-      <div className='suggestedVideos_videos'>
-        <Video/>
-        <Video/>
-        <Video/>
-        <Video/>
-        <Video/>
-        <Video/>
+  useEffect(() => {
+    getAllPopularVideos();
+  }, []);
 
+  const [popularVideos, setPopularVideos] = useState([]);
+
+  console.log(process.env.REACT_APP_API_KEY);
+
+  const getAllPopularVideos = async () => {
+    try {
+      let result = await axios.get(
+        `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=20&regionCode=US&key=AIzaSyBQhKr8cZztWfy65bHIfq6m-rAINaBZ4yc`
+      );
+
+      setPopularVideos(result.data.items);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+  console.log(popularVideos);
+
+  if (!popularVideos) {
+    console.log(popularVideos);
+    return <div>'loading...'</div>;
+  } else {
+    return (
+      <div className='suggested__videos'>
+        <div className='suggestedVideos_videos'>
+          {popularVideos.map((video) => {
+            return (
+              <Video
+                key={video.id}
+                image={video.snippet.thumbnails.high.url}
+                title={video.snippet.title}
+                channel={video.snippet.channelTitle}
+                views={video.statistics.viewCount}
+                uploadDate={video.snippet.publishedAt}
+              />
+            );
+          })}
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
 
-export default SuggestedVideos
+export default SuggestedVideos;
+// video.snippet.thumbnails.high
--- frontend/src/components/Video/Video.jsx
+++ frontend/src/components/Video/Video.jsx
@@ -1,21 +1,26 @@
-import react from "react";
-import './Video.css'
+import react from 'react';
+import moment from 'moment';
+import './Video.css';
 import Avatar from '@material-ui/core/Avatar';
 // TODO: setup up props to get thumbnail, title, channel, views and timestamp
 
 const Video = (props) => {
-    return (
-        <div className="videoContainer">
-            <img className = "videoThumbnail" src = "https://upload.wikimedia.org/wikipedia/commons/4/44/0000203ac_hippodroom_theater-circus.jpg"></img>
-            <div className="videoContainerInfo"> 
-                <Avatar/>
-                <div className="videoContainerText">
-                    <p className="videoTitleText"> title </p>
-                    <p className="videoViewsText"> channel <br></br> views · timestamp </p>
-                </div>
-            </div> 
+  return (
+    <div className='videoContainer'>
+      <img className='videoThumbnail' src={props.image}></img>
+      <div className='videoContainerInfo'>
+        <div className='videoContainerText'>
+          <p className='videoTitleText'>{props.title}</p>
+          <p className='videoViewsText'>
+            {' '}
+            {props.channel}
+            <br></br> views: {props.views} ·{' '}
+            {moment(props.uploadDate).startOf('day').fromNow()}
+          </p>
         </div>
-      );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default Video;
